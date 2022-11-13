4d5f6f63ff38b2b38f3d3b5f4193dd059a313793
Andrew Mullins <mullinsand15@gmail.com>
2022-11-13 15:50:47 -0700

    favorites table added with passing model tests for relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  # has_many :favorites
+  has_many :favorites
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,6 @@ RSpec.describe User, type: :model do
   end
 
   describe 'relationships' do
-    # it { should have_many :favorites }
+    it { should have_many :favorites }
   end
 end
