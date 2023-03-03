4d67c9ff4c0ca1e59f59e321516eae0be4f45ce7
Andrew Mullins <mullinsand15@gmail.com>
2023-03-03 12:22:08 -0700

    login user tests with controller, route updated

--- spec/requests/user_post_request_spec.rb
+++ spec/requests/user_post_request_spec.rb
@@ -45,4 +45,53 @@ RSpec.describe 'POST /api/v1/users' do
       end
     end
   end
+
+  describe 'login in an existing user' do
+    context 'request body contains a email and pswd' do
+      it 'full response when email exists and password is correct' do
+        @request_body = {  name: 'Arthur',
+                          email: 'argle@blargle.aaa',
+                          password: '123456',
+                          password_confirmation: '123456' }
+        @request_headers = { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+
+        post '/api/v1/users', headers: @request_headers, params: @request_body.to_json
+
+        @login_request_body = {  email: 'argle@blargle.aaa',
+                                 password: '123456'}
+
+        post '/api/v1/user', headers: @request_headers, params: @login_request_body.to_json
+
+        expect(response.status).to eq(201)
+        @user_response = json
+        expect(@user_response[:data]).to be_a(Hash)
+        expect(@user_response[:data][:id]).to be_a(String)
+        expect(@user_response[:data][:type]).to eq('user')
+        expect(@user_response[:data][:attributes]).to be_a(Hash)
+        expect(@user_response[:data][:attributes][:name]).to eq(@login_request_body[:name])
+        expect(@user_response[:data][:attributes][:email]).to eq(@login_request_body[:email])
+        expect(@user_response[:data][:attributes][:api_key]).to be_a(String)
+      end
+    end
+
+    context 'request contains an email that is not unique' do
+      it 'response contains an error message' do
+        @request_body = {  name: 'Arthur',
+          email: 'argle@blargle.aaa',
+          password: '123456',
+          password_confirmation: '123456' }
+  
+        @request_headers = { 'Content-Type': 'application/json',
+            'Accept': 'application/json' }
+  
+        post '/api/v1/users', headers: @request_headers, params: @request_body.to_json
+
+        post '/api/v1/users', headers: @request_headers, params: @request_body.to_json
+
+        expect(response.status).to eq(422)
+        @user_response = json
+        expect(@user_response[:errors]).to eq('Validation failed: Email has already been taken')
+      end
+    end
+  end
 end
